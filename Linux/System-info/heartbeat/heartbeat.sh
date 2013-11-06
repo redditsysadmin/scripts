a927dc6005c7e81263cd029c48e206d966294078
Joshua Terrill <joshterrill.dev@gmail.com>
2013-11-06 15:06:22 -0800

    updated heartbeat.sh with latest version

--- Linux/System-info/heartbeat/heartbeat.sh
+++ Linux/System-info/heartbeat/heartbeat.sh
@@ -1,18 +1,38 @@
-#!/bin/bash
+#! /bin/bash
 
-SERVER=google.com  #SERVER TO PING
-SID=  #ACCOUNT SID
-AUTH_TOKEN=  #AUTH TOKEN
-NUMBER=  #NUMBER REGISTERED WITH TWILIO
-RECIPIENT=  #SMS RECIPIENT
+SERVER=google.com  #SERVER TO PING - WILL BE OVERWRITTEN IF PROVIDED BY ARGUMENT
+RATE=5 #RATE TO CHECK THE SERVER (IN MINUTES) - WILL BE OVERWRITTEN IF PROVIDED BY ARGUMENT 
+SID= #ACCOUNT SID
+AUTH_TOKEN= #AUTH TOKEN
+NUMBER= #NUMBER REGISTERED WITH TWILIO
+RECIPIENT= #SMS RECIPIENT - WILL BE OVERWRITTEN IF PROVIDED BY ARGUMENT 
+
+while getopts s:r:t: opt; do
+  case $opt in
+  s)
+      SERVER=$OPTARG
+      ;;
+  r)
+      RATE=$OPTARG
+      ;;
+  t)
+      RECIPIENT=$OPTARG
+      ;;
+  esac
+done
 
 while true
 do
   if ping -c 1 $SERVER > /dev/null;
     then echo "Everything is OK at $(date)"
   else
-    echo "Server could not be reached at $(date)" | ./twilio-sms.sh -d $NUMBER -u $SID -p $AUTH_TOKEN $RECIPIENT
+    if [ "x${RECIPIENT}" != "x" ]
+    then
+      echo "Server could not be reached at $(date)" | ./twilio-sms.sh -d $NUMBER -u $SID -p $AUTH_TOKEN $RECIPIENT
+    else
+      echo "Server could not be reached at $(date), but no phone number was entered"
+    fi
     exit 0
   fi
-  sleep 300
+  sleep ${RATE}m 
 done
